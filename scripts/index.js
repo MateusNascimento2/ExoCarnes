49599e09fdc35f78d66c3fa2d13d4eeecb8a09b7
Mateus Nascimento <mateusnascar@hotmail.com>
2023-12-05 16:09:49 -0300

    Added mocks.js file to simulate a DB

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
+import { carnesExoticas, carnesNobres } from "./mocks.js";
+
 const headerButtonOpen = document.getElementById('c-navbar-button-open');
 const headerButtonClose = document.getElementById('c-navbar-button-close');
 const headerMobile = document.querySelector('.l-header-mobile');
 const linksMobile = document.querySelectorAll('.link-mobile');
+const exoticCardsContainer = document.querySelector('.l-ExoticMeat-cards');
+const primeCardsContainer = document.querySelector('.l-PrimeMeat-cards');
 
 headerButtonOpen.addEventListener('click', function() {
   headerMobile.classList.toggle('_is-open')
@@ -17,3 +21,78 @@ linksMobile.forEach((link) => {
   })
 })
 
+carnesExoticas.forEach((object) => {
+  const cardWrapper = document.createElement('div');
+  cardWrapper.classList.add('c-card-wrapper');
+
+  const imageWrapper = document.createElement('div');
+  imageWrapper.classList.add('l-meat-card-imageWrapper');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', object.imageSrc);
+  img.classList.add('c-card-img');
+  imageWrapper.appendChild(img);
+
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('c-card-content');
+
+  const h3 = document.createElement('h3');
+  h3.classList.add('c-content-title');
+  h3.textContent = object.nome;
+
+  const price = document.createElement('p');
+  price.classList.add('c-content-price');
+  price.textContent = `R$ ${object.preco}`;
+
+  const buttonBuy = document.createElement('a');
+  buttonBuy.classList.add('c-content-buy-button');
+  buttonBuy.textContent = "COMPRE AGORA";
+  buttonBuy.setAttribute('href', '#');
+
+
+  cardContent.appendChild(h3);
+  cardContent.appendChild(price);
+  cardContent.appendChild(buttonBuy);
+  cardWrapper.appendChild(imageWrapper);
+  cardWrapper.appendChild(cardContent);
+
+  exoticCardsContainer.appendChild(cardWrapper);
+});
+
+carnesNobres.forEach((object) => {
+  const cardWrapper = document.createElement('div');
+  cardWrapper.classList.add('c-card-wrapper');
+
+  const imageWrapper = document.createElement('div');
+  imageWrapper.classList.add('l-meat-card-imageWrapper');
+
+  const img = document.createElement('img');
+  img.setAttribute('src', object.imageSrc);
+  img.classList.add('c-card-img');
+  imageWrapper.appendChild(img);
+
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('c-card-content');
+
+  const h3 = document.createElement('h3');
+  h3.classList.add('c-content-title');
+  h3.textContent = object.nome;
+
+  const price = document.createElement('p');
+  price.classList.add('c-content-price');
+  price.textContent = `R$ ${object.preco}`;
+
+  const buttonBuy = document.createElement('a');
+  buttonBuy.classList.add('c-content-buy-button');
+  buttonBuy.textContent = "COMPRE AGORA";
+  buttonBuy.setAttribute('href', '#');
+
+
+  cardContent.appendChild(h3);
+  cardContent.appendChild(price);
+  cardContent.appendChild(buttonBuy);
+  cardWrapper.appendChild(imageWrapper);
+  cardWrapper.appendChild(cardContent);
+
+  primeCardsContainer.appendChild(cardWrapper);
+});
---
+++ scripts/mocks.js
@@ -0,0 +1,75 @@
+export const carnesExoticas = [
+  {
+    imageSrc: './images/bife_wagyu.jpg',
+    nome: 'Bife Ancho Wagyu',
+    preco: '695,00'
+  },
+
+  {
+    imageSrc: './images/t-bone.jpg',
+    nome: 'T-Bone Black Angus',
+    preco: '97,50'
+  },
+
+  {
+    imageSrc: './images/carne_jacare.png',
+    nome: 'Filé da Cauda de Jacaré',
+    preco: '79,90'
+  },
+
+  {
+    imageSrc: './images/tomahawk.png',
+    nome: 'PRIME RIBS TOMAHAWK',
+    preco: '60,00'
+  },
+
+  {
+    imageSrc: './images/carne_avestruz.png',
+    nome: 'CORTES DE AVESTRUZ',
+    preco: '40,00'
+  },
+
+  {
+    imageSrc: './images/picanha_javali.jpg',
+    nome: 'PICANHA DE JAVALI',
+    preco: '149,90'
+  },
+]
+
+export const carnesNobres = [
+  {
+    imageSrc: './images/linguica-costela.jpg',
+    nome: 'Linguiça de Costela Angus',
+    preco: '66,90'
+  },
+
+  {
+    imageSrc: './images/picanha_angus.jpg',
+    nome: 'Picanha Angus',
+    preco: '139,90'
+  },
+
+  {
+    imageSrc: './images/bife_chorizo.jpg',
+    nome: 'Bife Chorizo Angus',
+    preco: '65,90'
+  },
+
+  {
+    imageSrc: './images/babybeef.png',
+    nome: 'Baby Beef',
+    preco: '120,00'
+  },
+
+  {
+    imageSrc: './images/bife_ancho.webp',
+    nome: 'Bife Ancho Maturatta',
+    preco: '90,00'
+  },
+
+  {
+    imageSrc: './images/bife_cupim.avif',
+    nome: 'Cupim',
+    preco: '60,00'
+  },
+]
